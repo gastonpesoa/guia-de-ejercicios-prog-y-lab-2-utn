aeb27de7a088193a9f4c3930eedbd6b1b411f3d1
Gaston Pesoa <gastonpesoa@gmail.com>
2018-08-26 23:35:27 -0300

    Se inicia ejercicio 7, dias vividos, pendiente a resolver

---
+++ ejercicio1/Ej_07/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej_07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ej_07_Dias_Vividos";
+
+            int dia, mes, anio;
+            int diasVividos = 0;
+            string diaStr, mesStr, anioStr;
+            DateTime fechaActual = DateTime.Now;
+
+            Console.WriteLine("Fecha de nacimiento");
+            Console.Write("Ingrese dia: ");
+            diaStr = Console.ReadLine();
+            Console.Write("Ingrese mes: ");
+            mesStr = Console.ReadLine();
+            Console.Write("Ingrese anio: ");
+            anioStr = Console.ReadLine();
+
+            if((int.TryParse(diaStr, out dia)) && (int.TryParse(mesStr, out mes)) && (int.TryParse(anioStr, out anio)))
+            {
+                //for () { }
+            }
+
+        }
+    }
+}
