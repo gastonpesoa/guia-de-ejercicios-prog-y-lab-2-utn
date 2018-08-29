fc7a1e54410d018f65871b0556007916bcae9b47
Gaston Pesoa <gastonpesoa@gmail.com>
2018-08-29 10:06:26 -0300

    Se inicia Ej_13 (Conversor Decimal Binario) pendiente a resolver

---
+++ ejercicio1/Ej_13/Conversor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej_13
+{
+    class Conversor
+    {
+        public static string DecimalBinario(double numeroDecimal)
+        {
+            string numeroBinario = "";
+            double cociente = numeroDecimal / 2;
+            double resto = numeroDecimal % 2;
+
+            while (cociente > 0)
+            {
+
+                if (resto > 0)
+                {
+                    numeroBinario = numeroBinario + "1";
+                }
+                else
+                {
+                    if(resto == 0)
+                    {
+                        numeroBinario = numeroBinario + "0";
+                    }
+                }
+                cociente = cociente / 2;
+                resto = cociente % 2;
+            }
+            return numeroBinario;
+        } 
+    }
+}
