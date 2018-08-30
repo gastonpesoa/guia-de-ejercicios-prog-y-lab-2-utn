1771fafcedc43ca22db069871c7f129adff25527
alumno <alumno>
2018-08-30 20:29:43 -0300

    Se inicia Ej_17_Class-Boligrafo

---
+++ ejercicio1/Ej_17/Boligrafo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej_17
+{
+    class Boligrafo
+    {
+        public const short cantidadTintaMaxima = 100;
+        private ConsoleColor color;
+        private short tinta;
+
+        public Boligrafo(short tinta,ConsoleColor color)
+        {
+            this.tinta = tinta;
+            this.color = color;
+        }
+
+        public ConsoleColor GetColor()
+        {
+            return this.color;
+        }
+
+        public short GetTinta()
+        {
+            return this.tinta;
+        }
+
+        private void SetTinta(short tinta)
+        {
+            if(tinta >=0 && tinta <= cantidadTintaMaxima)
+            {
+                this.tinta = tinta;
+            }
+        }
+
+        public void Recargar() { }
+    }
+}
