4ea298028dd0be6f2aaca839fcd11c7b35a5a8c7
alumno <alumno>
2018-08-30 19:53:02 -0300

    Se resuelve Ej_16_Class_Alumno

--- ejercicio1/Ej_16/Alumno.cs
+++ ejercicio1/Ej_16/Alumno.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace Ej_16
 {
-    class Alumno
+    public class Alumno
     {
+        
         private byte nota1;
         private byte nota2;
         private float notaFinal;
@@ -15,15 +17,44 @@ namespace Ej_16
         public int legajo;
         public string nombre;
 
+        public Alumno(string nombre, string apellido, int legajo)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.legajo = legajo;
+        }
+
         public void CalcularFinal()
         {
-            notaFinal = (nota1 + nota2) / 2;
+            Thread.Sleep(150);
+            Random random = new Random();
+            this.notaFinal = -1;
+            if(this.nota1 >= 4 && this.nota2 >= 4)
+            {
+                this.notaFinal = random.Next(1,10);
+            }
         }
 
         public void Estudiar(byte nota1, byte nota2)
         {
-            Console.Write("Ingrese nota1: ");
-            nota1 = Convert.T Console.ReadLine();
+            this.nota1 = nota1;
+            this.nota2 = nota2;
+        }
+
+        public string Mostrar()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            if(this.notaFinal == -1)
+            {
+                stringBuilder.AppendFormat("{0} {1}\nLegajo: {2}\nNota 1: {3} Nota 2: {4}\nNota Final: {5}", this.nombre, this.apellido, this.legajo, this.nota1, this.nota2, "Alumno desaprobado");
+            }
+            else
+            {
+                stringBuilder.AppendFormat("{0} {1}\nLegajo: {2}\nNota 1: {3} Nota 2: {4}\nNota Final: {5}", this.nombre, this.apellido, this.legajo, this.nota1, this.nota2, this.notaFinal);
+            }
+            
+            return stringBuilder.ToString();
         }
     }
 }
--- ejercicio1/Ej_16/Program.cs
+++ ejercicio1/Ej_16/Program.cs
@@ -10,6 +10,25 @@ namespace Ej_16
     {
         static void Main(string[] args)
         {
+            Console.Title = "Ej_16_Class_Alumno";
+
+            Alumno alumno1 = new Alumno("Juan", "Roca", 00001);
+            Alumno alumno2 = new Alumno("Sergio", "Montes", 00002);
+            Alumno alumno3 = new Alumno("Raul", "Viana", 00003);
+
+            alumno1.Estudiar(4, 6);
+            alumno2.Estudiar(8, 9);
+            alumno3.Estudiar(2, 7);
+
+            alumno1.CalcularFinal();
+            alumno2.CalcularFinal();
+            alumno3.CalcularFinal();
+
+            Console.WriteLine(alumno1.Mostrar());
+            Console.WriteLine(alumno2.Mostrar());
+            Console.WriteLine(alumno3.Mostrar());
+
+            Console.ReadKey();
         }
     }
 }
