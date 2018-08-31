14c438f982b763731e7c713d8258418dc59604b0
alumno <alumno>
2018-08-30 22:16:28 -0300

    Se inicia Ej_17_Class_Boligrafo

--- ejercicio1/Ej_17/Boligrafo.cs
+++ ejercicio1/Ej_17/Boligrafo.cs
@@ -12,22 +12,40 @@ namespace Ej_17
         private ConsoleColor color;
         private short tinta;
 
+        /// <summary>
+        /// Constructor de la clase Boligrafo
+        /// </summary>
+        /// <param name="tinta"></param>
+        /// <param name="color"></param>
         public Boligrafo(short tinta,ConsoleColor color)
         {
             this.tinta = tinta;
             this.color = color;
         }
 
+        /// <summary>
+        /// Obtiene el color de la tinta del boligrafo
+        /// </summary>
+        /// <returns></returns>
         public ConsoleColor GetColor()
         {
             return this.color;
         }
 
+        /// <summary>
+        /// Obtiene el nivel de tinta del boligrafo
+        /// </summary>
+        /// <returns></returns>
         public short GetTinta()
         {
             return this.tinta;
         }
 
+        /// <summary>
+        /// Asigna el nivel de tinta del boligrafo validando que sea mayor a cero
+        /// y menor a la capacidad maxima
+        /// </summary>
+        /// <param name="tinta"></param>
         private void SetTinta(short tinta)
         {
             if(tinta >=0 && tinta <= cantidadTintaMaxima)
@@ -35,7 +53,39 @@ namespace Ej_17
                 this.tinta = tinta;
             }
         }
+        
+        /// <summary>
+        /// Asigna la cantidad maxima de tinta al boligrafo
+        /// </summary>
+        public void Recargar()
+        {
+            this.SetTinta(cantidadTintaMaxima);
+        }
 
-        public void Recargar() { }
+        /// <summary>
+        /// Consume el nivel de tinta, verifica que el resto no sea menor a cero
+        /// y dibuja mediante tantos '*' como haya podido gastar 
+        /// </summary>
+        /// <param name="gasto"></param>
+        /// <param name="dibujo"></param>
+        /// <returns></returns>
+        public bool Pintar(int gasto, out string dibujo)
+        {
+            bool returnAux = false;
+            dibujo = "";
+            int resto = this.GetTinta() - (short)gasto;
+            if (resto > 0)
+            {
+                this.SetTinta((short)resto);
+                returnAux = true;
+
+                for(int i = 1; i <= gasto; i++)
+                {
+                    dibujo += '*';  
+                }
+            }
+
+            return returnAux;
+        }
     }
 }
--- ejercicio1/Ej_17/Program.cs
+++ ejercicio1/Ej_17/Program.cs
@@ -10,6 +10,41 @@ namespace Ej_17
     {
         static void Main(string[] args)
         {
+            Console.Title = "Ej_17_Class_Boligrafo";
+
+            Boligrafo boligrafo1 = new Boligrafo(100, ConsoleColor.Blue);
+            Boligrafo boligrafo2 = new Boligrafo(50, ConsoleColor.Red);
+            string dibujo;
+
+            Console.WriteLine("Dibujo de boligrafo1");
+            if(boligrafo1.Pintar(50, out dibujo))
+            {
+                Console.ForegroundColor = boligrafo1.GetColor();
+                Console.WriteLine(dibujo);
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Nivel de tinta restante: {0}", boligrafo1.GetTinta());
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Nivel de tinta requerido superior a la capacidad disponible");
+            }
+
+            Console.Clear();
+            Console.WriteLine("Dibujo de boligrafo2");
+            if (boligrafo2.Pintar(30, out dibujo))
+            {
+                Console.ForegroundColor = boligrafo2.GetColor();
+                Console.WriteLine(dibujo);
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Nivel de tinta restante: {0}", boligrafo2.GetTinta());
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Nivel de tinta requerido superior a la capacidad disponible");
+            }
+
         }
     }
 }
