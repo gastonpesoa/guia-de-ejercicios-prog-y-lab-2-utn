b83b8d2c21db5a459cbff3f77886069913b75a61
Gaston Pesoa <gastonpesoa@gmail.com>
2018-09-02 20:31:18 -0300

    Se inicia y crea proyecto Ej_18_Punto_Rectangulo

---
+++ ejercicio1/Ej_18/Rectangulo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej_18
+{
+    class Rectangulo
+    {
+        private Punto vertice1;
+        private Punto vertice2;
+        private Punto vertice3;
+        private Punto vertice4;
+        private float area;
+        private float perimetro;
+
+        public Rectangulo(Punto vertice1, Punto vertice3)
+        {
+            
+        }
+    }
+}
