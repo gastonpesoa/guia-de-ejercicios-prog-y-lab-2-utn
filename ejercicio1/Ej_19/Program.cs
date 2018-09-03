301b48b1274b8a6ee0648d0c5a17efd262309549
Gaston Pesoa <gastonpesoa@gmail.com>
2018-09-02 21:03:31 -0300

    Se inicia proyecto Ej_19_Sumadores y se resuelve

---
+++ ejercicio1/Ej_19/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej_19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ej_19_Sumador";
+
+            Sumador sumador1 = new Sumador();
+            Sumador sumador2 = new Sumador(7);
+
+            Console.WriteLine(sumador1.Sumar("a", "b"));
+            Console.WriteLine(sumador1.Sumar(1,2));
+
+            Console.WriteLine("Cantidad sumas sumador1 {0}",(int)sumador1);
+            Console.WriteLine("Cantidad sumas sumador2 {0}", (int)sumador2);
+
+            Console.WriteLine("Suma de sumadores {0}",sumador1 + sumador2);
+            Console.WriteLine("Igualdad de sumador1 y sumador2 {0}", sumador1 | sumador2);
+            Console.WriteLine("Igualdad de sumador1 y sumador1 {0}", sumador1 | sumador1);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ejercicio1/Ej_19/Sumador.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej_19
+{
+    class Sumador
+    {
+        private int cantidadSumas;
+
+        public Sumador()
+        {
+            this.cantidadSumas = 0;
+        }
+
+        public Sumador(int cantidadSumas)
+        {
+            this.cantidadSumas = cantidadSumas;
+        }
+
+        public long Sumar(long a, long b)
+        {
+            this.cantidadSumas++;
+            return a + b;
+        }
+
+        public string Sumar(string a, string b)
+        {
+            this.cantidadSumas++;
+            return a + b;
+        }
+
+        public static explicit operator int(Sumador sumador)
+        {
+            return sumador.cantidadSumas;
+        }
+
+        public static long operator +(Sumador sumador1, Sumador sumador2)
+        {
+            return sumador1.cantidadSumas + sumador2.cantidadSumas;
+        }
+
+        public static bool operator |(Sumador sumador1, Sumador sumador2)
+        {
+            return (sumador1.cantidadSumas == sumador2.cantidadSumas);
+        }
+    }
+
+}
