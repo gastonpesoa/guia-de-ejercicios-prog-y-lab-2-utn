4e8ebbfef09b58e0d6df0f85ccafe5acc1eff957
Gaston Pesoa <gastonpesoa@gmail.com>
2018-09-02 23:57:31 -0300

    Se crea proyecto e inicia Ej_20_PesosDolarEuro

---
+++ ejercicio1/Ej_20/Dolar.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej_20
+{
+    class Dolar
+    {
+        private double cantidad;
+        private static float cotizRespectoDolar;
+
+        private Dolar()
+        {
+             
+        }
+
+    }
+}
---
+++ ejercicio1/Ej_20/Pesos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej_20
+{
+    class Pesos
+    {
+        private double cantidad;
+        private static float cotizRespectoDolar;
+    }
+}
