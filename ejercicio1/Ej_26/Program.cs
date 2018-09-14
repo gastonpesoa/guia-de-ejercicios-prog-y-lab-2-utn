cf7517ba69196bd495ec5fd680c371ab5e3fc52a
alumno <alumno>
2018-09-13 22:11:49 -0300

    Se agregan ejercicios 27 y 28

--- ejercicio1/Ej_26/Program.cs
+++ ejercicio1/Ej_26/Program.cs
@@ -8,8 +8,69 @@ namespace Ej_26
 {
     class Program
     {
+        private static int[] OrdenarArray(int [] arrayNumerosAleatorios)
+        {
+            for (int i = 0; i < arrayNumerosAleatorios.Length - 1; i++)
+            {
+                for (int j = i + 1; j < arrayNumerosAleatorios.Length; j++)
+                {
+                    if (arrayNumerosAleatorios[i] > 0 && arrayNumerosAleatorios[i] < arrayNumerosAleatorios[j])
+                    {
+                        int aux = arrayNumerosAleatorios[i];
+                        arrayNumerosAleatorios[i] = arrayNumerosAleatorios[j];
+                        arrayNumerosAleatorios[j] = aux;
+                    }
+                    if (arrayNumerosAleatorios[i] < 0 && arrayNumerosAleatorios[i] > arrayNumerosAleatorios[j])
+                    {
+                        int aux = arrayNumerosAleatorios[i];
+                        arrayNumerosAleatorios[i] = arrayNumerosAleatorios[j];
+                        arrayNumerosAleatorios[j] = aux;
+                    }
+                }
+            }
+            return arrayNumerosAleatorios;
+        }
+
         static void Main(string[] args)
         {
+            Console.Title = "Ej_26_Carga_Aleatoria_de_Array";
+            int[] arrayNumerosAleatorios;
+            arrayNumerosAleatorios = new int[20];
+
+            Random random = new Random();
+
+            for(int i = 0; i< arrayNumerosAleatorios.Length; i++)
+            {
+                arrayNumerosAleatorios[i] = random.Next(-200, 200);
+            }
+
+            Console.WriteLine("Array generado: ");
+            foreach (int item in arrayNumerosAleatorios)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("Ordenando Array");
+            Program.OrdenarArray(arrayNumerosAleatorios);
+
+            Console.WriteLine("Positivos en forma decreciente: ");
+            foreach (int item in arrayNumerosAleatorios)
+            {
+                if (item > 0)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+
+            Console.WriteLine("Negativos en forma creciente: ");
+            foreach (int item in arrayNumerosAleatorios)
+            {
+                if (item < 0)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
---
+++ ejercicio1/Ej_28_Diccionarios/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace Ej_28_Diccionarios
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(209, 248);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 0;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(272, 230);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(296, 283);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnCalcular);
+            this.Name = "Form1";
+            this.Text = "Contador de Palabras";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
