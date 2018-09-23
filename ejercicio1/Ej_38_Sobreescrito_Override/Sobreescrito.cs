2b0b6cb5dc08c31944a79799e34840e3ead1af86
Gaston Pesoa <gastonpesoa@gmail.com>
2018-09-23 12:21:30 -0300

    Se agrega Ej_38_Sobreescrito

---
+++ ejercicio1/Ej_38_Sobreescrito_Override/Sobreescrito.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej_38_Sobreescrito_Override
+{
+    public class Sobreescrito
+    {
+        public override string ToString()
+        {
+            return "\n¡Este es mi método ToString sobreescrito!\n"; 
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Sobreescrito;    
+        }
+
+        public override int GetHashCode()
+        {
+            return 1142510187;  
+        }
+    }
+}
