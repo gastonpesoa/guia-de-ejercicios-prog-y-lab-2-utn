317f727004fba5c1e87eace086edb6ef0b79fcce
alumno <alumno>
2018-10-17 22:06:48 -0300

    Se agrega Ej_44 Test_Unitarios

---
+++ ejercicio1/ValidaListaDeLlamadasCentralita/CentralitaTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Centralita_Biblioteca;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ValidaListaDeLlamadasCentralita
+{
+    [TestClass]
+    public class CentralitaTest
+    {
+        [TestMethod]
+        //[ExpectedException(typeof(AssertFailedException))]
+        public void TestListaDeLlamadasInciada()
+        {
+            #region "Arrange - Act"
+            Centralita centralita = new Centralita("Centralita List Test");
+            #endregion
+
+            #region "Assert"
+            //Assert.IsNull(centralita.Llamadas);
+            try
+            {
+                Assert.IsNull(centralita.Llamadas);
+            }
+            catch (AssertFailedException ex)
+            {
+                Assert.IsInstanceOfType(ex, typeof(AssertFailedException));
+            }
+            #endregion
+        }
+
+        [TestMethod]
+        //[ExpectedException(typeof(CentralitaException))]
+        public void TestCentralitaException()
+        {
+            #region "Arrange"
+            Centralita centralita = new Centralita("Centralita Exception Test");
+            Local l1 = new Local("Bernal", 15, "Lanus", 5.3F);
+            Local l2 = new Local("Bernal", 15, "Lanus", 5.3F);
+            Local l3 = new Local("Quilmes", 3.6F, "Avellaneda", 5.3F);
+            #endregion
+
+            //centralita += l1;
+            //centralita += l2;
+            //centralita += l3;
+            centralita += l1;
+            try
+            {
+                centralita += l2;
+            }
+            catch(CentralitaException ex)
+            {
+                Assert.IsInstanceOfType(ex, typeof(CentralitaException));
+            }
+            centralita += l3;
+        }
+    }
+}
---
+++ ejercicio1/ValidaListaDeLlamadasCentralita/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ValidaListaDeLlamadasCentralita")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("ValidaListaDeLlamadasCentralita")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("737171e1-d856-42bf-92f4-255d7e1f0bc5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
